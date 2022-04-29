9c145fc5af5fb173a3ba17720b54460944ff3394
wilberC <wilber.carrascal@gmail.com>
2022-04-29 03:43:48 -0500

    add-django-variable-config

--- django_template/urls.py
+++ django_template/urls.py
@@ -1,4 +1,4 @@
-"""django_template URL Configuration
+""" URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
--- django_template/wsgi.py
+++ django_template/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for django_template project.
+WSGI config for project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_template.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', f'{os.getenv("PROJECT_NAME")}.settings')
 
 application = get_wsgi_application()
